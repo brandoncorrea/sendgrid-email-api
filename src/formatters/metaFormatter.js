c43c03d193c1ff918d558690a9175fcff43d4bb5
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-07-14 17:06:45 -0400

    Add Author Formatting

--- src/formatters/metaFormatter.js
+++ src/formatters/metaFormatter.js
@@ -1,3 +1,12 @@
+const formatAuthor = author => {
+  var parts = author.split(' ');
+  for (var i = 0; i < parts.length; i++)
+    parts[i] = parts[i][0].toUpperCase() + parts[i].substring(1);
+  return parts.join(' ');
+}
+
 exports.format = (author, date) => {
+  if (author && author.trim()) 
+    return formatAuthor(author);
   return '';
 }
--- test/metaFormatter.test.js
+++ test/metaFormatter.test.js
@@ -25,4 +25,39 @@ describe("Formats Note Meta Content from Request Object", () => {
     it("Is empty: whitespace author and undefined date", () => 
       assertCreatesEmptyString(' ', undefined))
   })
+
+  describe("Test when Author has value and Date is empty", () => {
+    const assertCreatesAuthorString = (author, date) => 
+      expect(metaFormatter.format(author, date)
+    ).to.equal(author)
+
+    it("Is 'FirstName' Author and null Date", () =>
+      assertCreatesAuthorString("FirstName", null))
+    it("Is 'FirstName' Author and undefined Date", () =>
+      assertCreatesAuthorString("FirstName", undefined))
+    it("Is 'First Last' Author and null Date", () => 
+      assertCreatesAuthorString("First Last", null))
+    it("Is 'First Last' Author and undefined Date", () => 
+      assertCreatesAuthorString("First Last", undefined))
+  })
+
+  describe("Test author name formatting", () => {
+    const assertAuthorEquals = (author, expected) =>
+      expect(
+        metaFormatter.format(author, null)
+      ).to.equal(expected)
+
+    it("Capitalizes first character", () =>
+      assertAuthorEquals('firstName', 'FirstName'))
+    it("Capitalizes All Names", () =>
+      assertAuthorEquals('funny little bunny', 'Funny Little Bunny'))
+    it("Capitalizes Single Character Words", () =>
+      assertAuthorEquals('first s third', 'First S Third'))
+    it("Capitalizes Single Character", () =>
+      assertAuthorEquals('a', 'A'))
+    it("Capitalizes Names that are already Capitalized", () =>
+      assertAuthorEquals('This Is Capitalized', 'This Is Capitalized'))
+    it("Retains capitalization in the middle of a name", () =>
+      assertAuthorEquals('mcFarley mcGregor macBeth', 'McFarley McGregor MacBeth'))
+  })
 })
