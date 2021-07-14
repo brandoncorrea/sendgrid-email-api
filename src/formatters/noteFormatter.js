ab0d5b0220c942b81060ee52dc22849ec04a7a6d
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-07-14 14:18:45 -0400

    Refactor

--- src/formatters/noteFormatter.js
+++ src/formatters/noteFormatter.js
@@ -4,27 +4,11 @@
 
 const htmlHelper = require("../helpers/htmlHelper");
 
-// Replaces an element with its value property
-const replaceWithValue = (html, element) => {
-  if (element.hasAttribute('value'))
-    htmlHelper.replaceElement(html, element, element.value);
-  else
-    htmlHelper.removeElement(element);
-}
-
-// Replaces an elemnt with its inner HTML
-const replaceWithInnerHtml = (html, element) => {
-  if (element.innerHTML !== '')
-    htmlHelper.replaceElement(html, element, element.innerHTML);
-  else
-    htmlHelper.removeElement(element);
-}
-
 // Formats an HTML string for an emailed note
 exports.format = htmlString => {
   if (!htmlString) htmlString = "";
   var html = htmlHelper.parseHtml(htmlString);
-  htmlHelper.applyElements(html, 'input', replaceWithValue);
-  htmlHelper.applyElements(html, 'textarea', replaceWithInnerHtml);
+  htmlHelper.replaceElements(html, 'input', el => el.value);
+  htmlHelper.replaceElements(html, 'textarea', el => el.innerHTML);
   return html.body.innerHTML;
 }
--- src/helpers/htmlHelper.js
+++ src/helpers/htmlHelper.js
@@ -1,14 +1,18 @@
 const jsdom = require("jsdom");
 
-// Replaces an element with text content
-exports.replaceElement = (html, element, content) => 
-  element.parentElement.replaceChild(
-    html.createTextNode(content), 
-    element);
+// Converts a string to an HTML document
+exports.parseHtml = xmlString =>
+  (new jsdom.JSDOM(xmlString)).window.document;
 
 // Removes an element from its parent
 exports.removeElement = element => 
   element.parentElement.removeChild(element);
+
+// Replaces an element with text content
+exports.replaceElement = (html, element, content) =>
+  element.parentElement.replaceChild(
+    html.createTextNode(content), 
+    element);
 
 // Applies a method to all elements with a given tag name
 exports.applyElements = (html, tagName, apply) => {
@@ -19,6 +23,16 @@ exports.applyElements = (html, tagName, apply) => {
   }
 }
 
-// Converts a string to an HTML document
-exports.parseHtml = xmlString =>
-  (new jsdom.JSDOM(xmlString)).window.document;
+// Replaces an HTML element with the content
+exports.replaceContent = (html, element, content) => {
+  if (content)
+    this.replaceElement(html, element, content);
+  else
+    this.removeElement(element);
+}
+
+// Replaces HTML elements with the tag name using a value based on the selector
+exports.replaceElements = (html, tagName, selector) => 
+  this.applyElements(html, tagName, 
+    (doc, el) => this.replaceContent(doc, el, selector(el)))
+
