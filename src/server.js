173c510347fff47aa57e7225fa003ca337030914
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-07-14 19:56:15 -0400

    Update port to use environment

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 var http = require('http');
 
+var port = process.env.PORT || 8080;
+
 //create a server object:
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(port); //the server object listens on port 8080
