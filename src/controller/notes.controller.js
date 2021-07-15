5316bce546721ed85de6a55a49e397309e191a84
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-07-15 01:39:39 -0400

    Update controller with request body

--- src/controller/notes.controller.js
+++ src/controller/notes.controller.js
@@ -1,15 +1,27 @@
-var noteBuilder = require('../builders/sermonNoteBuilder');
-var sendgrid = require('../services/sendgrid.service');
-
+const noteBuilder = require('../builders/sermonNoteBuilder');
+const sendgrid = require('../services/sendgrid.service');
+var validator = require("email-validator");
+ 
 module.exports.send = (req, res) => {
+  var body = req.body;
+
+  if (!validator.validate(body.recipient)) {
+    res.status(400);
+    res.send('bad request');
+    return;
+  }
+
   sendgrid.sendMail(
-    'bwancor@gmail.com',
-    'test@example.com',
-    'Test Email',
-    '<div>Content</div>'
+    body.recipient,
+    body.title,
+    noteBuilder.build(body).documentElement.innerHTML
   )
-  .then(response => res.status(200))
+  .then(response => {
+    console.log('response', response);
+    res.status(200)
+  })
   .catch(err => {
+    console.error(err);
     res.status(500);
     res.send(err);
   });
